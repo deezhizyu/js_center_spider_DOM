cd06b99c36a427c6f470ab47bc7b8413856968ce
deezhizyu <kalenskuja@gmail.com>
2024-06-13 14:43:47 +0200

    make build

---
+++ index.d6db1724.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),p=getComputedStyle(e),r=parseInt(p.height),l=parseInt(p.width),o=getComputedStyle(t),a=parseInt(o.height),c=parseInt(o.width);t.style.top="".concat(r/2-a/2,"px"),t.style.left="".concat(l/2-c/2,"px");
+//# sourceMappingURL=index.d6db1724.js.map
